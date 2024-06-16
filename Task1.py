1e0391d51273cb5e6eb29a62935ae667060fec8f
Kellyz <124003327+Kelly-Zeng-yuxi@users.noreply.github.com>
2024-06-16 15:37:34 -0400

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -37,12 +37,8 @@ print(user_rows)
 # Extract unique user IDs for potential customers (label == 1) from ads_df
 potential_customers_ids = ads_df[ads_df['label'] == 1]['user_id'].unique()
 
-# Extract unique user IDs for non-potential customers (label == -1) from feeds_df
-non_potential_customers_ids = feeds_df[feeds_df['label'] == -1]['u_userId'].unique()
-
 # Filter the datasets for these users
 potential_customers_df = ads_df[ads_df['user_id'].isin(potential_customers_ids)].drop_duplicates(subset='user_id')
-non_potential_customers_df = ads_df[ads_df['user_id'].isin(non_potential_customers_ids)].drop_duplicates(subset='user_id')
 
 # Plot histograms for potential customers
 plt.figure(figsize=(17, 5))
@@ -69,93 +65,64 @@ plt.ylabel('Frequency')
 plt.tight_layout()
 plt.show()
 
-# Plot histograms for non-potential customers
-plt.figure(figsize=(17, 5))
-
-plt.subplot(1, 3, 1)
-non_potential_customers_df['age'].value_counts().plot(kind='bar')
-plt.title('Age Distribution (Non-Potential Customers)')
-plt.xlabel('Age')
-plt.ylabel('Frequency')
-
-plt.subplot(1, 3, 2)
-non_potential_customers_df['residence'].value_counts().plot(kind='bar')
-plt.title('Residence Distribution (Non-Potential Customers)')
-plt.xlabel('Residence')
-plt.ylabel('Frequency')
-
-plt.subplot(1, 3, 3)
-top_devices = non_potential_customers_df['device_name'].value_counts().head(10)
-top_devices.plot(kind='bar')
-plt.title('Top 10 Device Name Distribution (Non-Potential Customers)')
-plt.xlabel('Device Name')
-plt.ylabel('Frequency')
-
-plt.tight_layout()
-plt.show()
 
 
-#Content Analysis
+#%% Engagement: hour, day of the week, day of the month
+
+# Convert 'pt_d' to datetime
+potential_customers_df['pt_d'] = pd.to_datetime(potential_customers_df['pt_d'], format='%Y%m%d%H%M')
+
+# Extract hour, day of the week, and day of the month
+potential_customers_df['hour'] = potential_customers_df['pt_d'].dt.hour
+potential_customers_df['day_of_week'] = potential_customers_df['pt_d'].dt.dayofweek
+potential_customers_df['day_of_month'] = potential_customers_df['pt_d'].dt.day
 
 # Plot histograms for potential customers
 plt.figure(figsize=(17, 5))
 
+plt.subplot(1, 3, 1)
+potential_customers_df['hour'].value_counts().sort_index().plot(kind='bar')
+plt.title('Hourly Distribution (Potential Customers)')
+plt.xlabel('Hour of the Day')
+plt.ylabel('Frequency')
+
+plt.subplot(1, 3, 2)
+potential_customers_df['day_of_week'].value_counts().sort_index().plot(kind='bar')
+plt.title('Day of Week Distribution (Potential Customers)')
+plt.xlabel('Day of the Week')
+plt.ylabel('Frequency')
+
+plt.subplot(1, 3, 3)
+potential_customers_df['day_of_month'].value_counts().sort_index().plot(kind='bar')
+plt.title('Day of Month Distribution (Potential Customers)')
+plt.xlabel('Day of the Month')
+plt.ylabel('Frequency')
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+#%% Content
+
+# Plot histograms for potential customers
+plt.figure(figsize=(10, 5))
+
 plt.subplot(1, 2, 1)
-top_newsCatInsterestsST = potential_customers_df['u_newsCatInterestsST'].value_counts().head(10)
-top_newsCatInsterestsST.plot(kind='bar')
-plt.title('News Category Interests Distribution (Potential Customers)')
-plt.xlabel('News Category Interests')
+potential_customers_df['ad_click_list_v001'].value_counts().head(10).plot(kind='bar')
+plt.title('Top 10 Ad Clicks (Potential Customers)')
+plt.xlabel('Ad Task ID')
 plt.ylabel('Frequency')
 
 plt.subplot(1, 2, 2)
-top_newsCatInterestsST = non_potential_customers_df['u_newsCatInterestsST'].value_counts().head(10)
-top_newsCatInterestsST.plot(kind='bar')
-plt.title('Top News Category Interests Distribution (Non-Potential Customers)')
-plt.xlabel('News Category Interests')
+potential_customers_df['u_newsCatInterestsST'].value_counts().head(10).plot(kind='bar')
+plt.title('News Category Interests (Potential Customers)')
+plt.xlabel('News Category')
 plt.ylabel('Frequency')
+
 
 plt.tight_layout()
 plt.show()
 
-#%% SCRATCH
-plt.subplot(1, 3, 2)
-potential_customers_df['u_refreshTimes'].value_counts().plot(kind='bar')
-plt.title('Refresh Times Distribution (Potential Customers)')
-plt.xlabel('Refresh Times')
-plt.ylabel('Frequency')
-
-plt.subplot(1, 3, 3)
-potential_customers_df['u_feedLifeCycle'].value_counts().plot(kind='bar')
-plt.title('Feed Life Cycle Distribution (Potential Customers)')
-plt.xlabel('Feed Life Cycle')
-plt.ylabel('Frequency')
-
-plt.tight_layout()
-plt.show()
-
-
-
-plt.figure(figsize=(17, 5))
-
-plt.subplot(1, 3, 1)
-top_newsCatInterestsST = non_potential_customers_df['u_newsCatInterestsST'].value_counts().head(10)
-top_newsCatInterestsST.plot(kind='bar')
-plt.title('Top News Category Interests Distribution (Non-Potential Customers)')
-plt.xlabel('News Category Interests')
-plt.ylabel('Frequency')
-
-plt.subplot(1, 3, 2)
-non_potential_customers_df['u_refreshTimes'].value_counts().plot(kind='bar')
-plt.title('Refresh Times Distribution (Non-Potential Customers)')
-plt.xlabel('Refresh Times')
-plt.ylabel('Frequency')
-
-plt.subplot(1, 3, 3)
-non_potential_customers_df['u_feedLifeCycle'].value_counts().plot(kind='bar')
-plt.title('Feed Life Cycle Distribution (Non-Potential Customers)')
-plt.xlabel('Feed Life Cycle')
-plt.ylabel('Frequency')
-
-plt.tight_layout()
-plt.show()
 
